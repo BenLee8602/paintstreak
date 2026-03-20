267328eef31daa532e04bac4125d9b9f36a817da
Ben Lee <ben.lee8602@gmail.com>
2026-03-19 23:43:38 -0400

    form styling

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,13 @@
 function Login() {
-    return <>Login</>;
+    return <div className="form-wrapper">
+        <div className="form">
+            <label>email</label>
+            <input/>
+            <label>username</label>
+            <input/>
+            <button>submit</button>
+        </div>
+    </div>;
 }
 
 export default Login;
